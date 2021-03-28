f40744ec1f958724da4ff26e1c15f12b0d6515bb
BartlomiejMaj <bartlomiejmajzner@gmail.com>
2021-03-28 20:44:38 +0200

    My second commit

--- ArgumentsPrinterApp.java
+++ ArgumentsPrinterApp.java
@@ -3,9 +3,9 @@ package loop;
 public class ArgumentsPrinterApp {
     public static void main(String[] args) {
 
-        String[] arguments = {"AA", "BB", "CC", "DD"};
-        for (int i = 0; i < arguments.length; i++){
-            String arg = arguments[i];
+        //String[] arguments = {"AA", "BB", "CC", "DD"};
+        for (int i = 0; i < args.length; i++){
+            String arg = args[i];
             System.out.println(arg);
 
         }
--- SumOfNumbersApp.java
+++ SumOfNumbersApp.java
@@ -3,74 +3,37 @@ package loop;
 import java.util.Scanner;
 
 public class SumOfNumbersApp {
-    //public static int sum(int a, int b){
-      //  return a + b;
-   // }
-
-    public static void main(String[] args) {
-
-        Scanner number = new Scanner(System.in);
-        System.out.print("Please, enter first number: ");
-        String a = number.next();
-        System.out.print("Please, enter second number: ");
-        String b = number.next();
-        System.out.print("Please, enter third number: ");
-        String c = number.next();
-
-        int integerA = Integer.valueOf(a);
-        //integerA = Integer.parseInt(args[0]);
-
-        int integerB = Integer.valueOf(b);
-       // integerB = Integer.parseInt(args[0]);
-
-        int integerC = Integer.valueOf(c);
-        //integerC = Integer.parseInt(args[0]);
-
-        int sumOfAll = integerA + integerB + integerC;
-
-        System.out.println("Your sum: " + sumOfAll);
-
-        System.out.println("===========================================");
+      public static void main(String[] args) {
 
 
-        String d = "111";
-        String e = "222";
-        String f = "333";
+          int integerNumberA = Integer.parseInt("123");
+          integerNumberA = Integer.parseInt(args[0]);
 
-        int integerD = Integer.parseInt("111");
-        //int integerD = Integer.parseInt(args[0]);
+          int integerNumberB = Integer.parseInt("456");
+          integerNumberB = Integer.parseInt(args[1]);
 
-        int integerE = Integer.parseInt("222");
-        //int integerE = Integer.parseInt(args[0]);
+          int integerNumberC = Integer.parseInt("789");
+          integerNumberC = Integer.parseInt(args[2]);
 
-        int integerF = Integer.parseInt("333");
-        //int integerF = Integer.parseInt(args[0]);
+          int sumOfAllIntegers = integerNumberA + integerNumberB + integerNumberC;
 
-        int sumNextInts = integerD + integerE + integerF;
-        System.out.println(sumNextInts);
+          System.out.println(sumOfAllIntegers);
 
 
-        System.out.println("============================================");
+          System.out.println("==================================");
 
 
-        String g = "1.11";
-        String h = "2.22";
-        String i = "3.33";
+          double doubleNumberA = Double.parseDouble("1.23");
+          doubleNumberA = Double.parseDouble(args[3]);
 
-        double doubleG = Double.parseDouble("1.11");
-       //doubleG = Double.parseDouble(args[g]);
+          double doubleNumberB = Double.parseDouble("4.56");
+          doubleNumberB = Double.parseDouble(args[4]);
 
-        double doubleE = Double.parseDouble("2.22");
-        //double doubleE = Double.parseDouble(args[h]);
+          double doubleNumberC = Double.parseDouble("7.89");
+          doubleNumberC = Double.parseDouble(args[5]);
 
-        double doubleF = Double.parseDouble("3.33");
-        //double doubleF = Double.parseDouble(args[0]);
+          double sumOfAllDoubles = doubleNumberA + doubleNumberB + doubleNumberC;
 
-        double sumNextDouble = doubleG + doubleE + doubleF;
-        System.out.println(sumNextDouble);
-
-
-
-
+          System.out.println(sumOfAllDoubles);
     }
 }
