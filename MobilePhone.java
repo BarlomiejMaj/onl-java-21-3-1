2af51289c02c9e353807a325041ecd1f8ed559d8
BarlomiejMaj <81513730+BarlomiejMaj@users.noreply.github.com>
2021-04-11 21:34:01 +0200

    Fixing bugs in code

--- MobilePhone.java
+++ MobilePhone.java
@@ -8,45 +8,48 @@ public class MobilePhone {
     int batteryAmount;
     int memory;
     int freeMemory;
-    int hours;
+
 
     public MobilePhone(String brand, int batteryCapacity, int memory ){
         this.brand = brand;
         this.batteryCapacity = batteryCapacity;
         this.memory = memory;
-        this.batteryAmount = hours * 100;
+        this.batteryAmount = batteryCapacity;
+
 
     }
 
     public void installApplication(String appName, int appMemory) {
-        if (this.memory >= appMemory) {
-            this.memory = memory - appMemory;
-            System.out.printf("Application %s has been installed on %s. Remain memory: %s MB %n", appName, this.brand, this.memory);
+        if (memory >= appMemory) {
+            memory = memory - appMemory;
+            System.out.printf("Application %s has been installed on %s. Remain memory: %s MB %n", appName, brand, memory);
 
         } else {
-            System.out.printf("Application %s can't be installed on %s. There are no enough memory %n", appName, this.brand);
+            System.out.printf("Application %s can't be installed on %s. There are no enough memory %n", appName, brand);
         }
     }
 
 
     public void use(String appName, int hours) {
-        if (this.batteryCapacity >= hours * 100) {
-            this.batteryCapacity = batteryCapacity - hours * 100;
-            System.out.printf("Application %s has been ran for %s hours on %s. Remain battery capacity: %s mAh %n", appName, hours, this.brand, this.batteryCapacity);
-       // } else if (this.batteryCapacity == 0 ){
-       //     System.out.printf("Application %s can't be run on %s. Phone discharged %n", appName, this.brand);
-        } else {
-            this.batteryCapacity = batteryCapacity - hours * 100;
-            System.out.printf("Application %s has been used for %s hours on %s. Phone has been discharged. %n", appName, hours, this.brand);
-            if (this.batteryCapacity <= 0){
-                System.out.printf("Application %s can't be run on %s. Phone discharged %n", appName, this.brand);
+       if(batteryAmount > 0){
+            int discharge = hours * 100;
+            if (batteryAmount - discharge > 0) {
+                batteryAmount = batteryAmount - discharge;
+                System.out.printf("Application %s has been ran for %s hours on %s. Remain battery capacity: %s mAh %n", appName, hours, brand, batteryAmount);
+
+            } else {
+                batteryAmount = batteryAmount - discharge;
+                System.out.printf("Application %s has been used for %s hours on %s. Phone has been discharged. %n", appName, hours, brand);
             }
-        }
+       } else {
+           System.out.printf("Application %s can't be run on %s. Phone discharged %n", appName, brand);
+       }
     }
 
 
+
     public void charge(){
-        this.batteryCapacity = 1000;
-        System.out.printf("%s has been charged. Remain battery capacity: %s mAh %n", this.brand, this.batteryCapacity);
+        batteryAmount = batteryCapacity;
+        System.out.printf("%s has been charged. Remain battery capacity: %s mAh %n", brand, batteryAmount);
     }
 }
